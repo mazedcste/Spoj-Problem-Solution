73ad78a8d7ddce98f563cf2f884e57da49916e89
Md. Abdul Mazed Rupok <mazed4g@gmail.com>
2017-06-09 11:39:14 +0600

    dp

--- SAMER08D.cpp
+++ SAMER08D.cpp
@@ -14,15 +14,14 @@ int main ()
         tlen = strlen (t+1);
         for (int i = 1; i <= slen; i++) {
             for (int j = 1; j <= tlen; j++) {
-                dp[i][j] = max (dp[i][j-1], dp[i-1][j]);
+                dp[i][j] = max (dp[i][j-1], dp[i-1][j]); //just like LCS
                 for (int c = 0; c <= i; c++) {
-                    if (c >= k) dp[i][j] = max (dp[i][j], dp[i-c][j-c] + c);
+                    if (c >= k) dp[i][j] = max (dp[i][j], dp[i-c][j-c] + c); //how many in sequence previously
                     if (s[i-c] != t[j-c]) break;
                 }
             }
         }
         printf ("%d\n", dp[slen][tlen]);
     }
-
     return 0;
 }
